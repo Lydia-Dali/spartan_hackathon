eeda26d93ec01ce9f885c7dde5c2da41d82f5bda
lydia.dal@live.fr <lydia.dal@live.fr>
2018-12-19 16:34:18 +0100

    arborescence base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import { Provider } from "react-redux";
 
-import Router from './routes'
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Accueil from "./components/PageAccueil/Accueil";
+import Page1 from "./components/Page1/page1";
+import Page2 from "./components/Page2/page2";
+import Page3 from "./components/Page3/page3";
+import Page4 from "./components/Page4/page4";
+import Page5 from "./components/Page5/page5";
+import Page6 from "./components/Page6/page6";
+import Page7 from "./components/Page7/page7";
+import Page8 from "./components/Page8/page8";
+
+
+// import Router from './routes'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router/>
+      <div>
+      <Provider>
+        <BrowserRouter>
+          <div>            
+
+            <Switch>
+              <Route exact path="/" component={Accueil} />
+              <Route path="/Page1" component={Page1} />
+              <Route path="/Page2" component={Page2} />
+              <Route path="/Page3" component={Page3} />
+              <Route path="/Page4" component={Page4} />
+              <Route path="/Page5" component={Page5} />
+              <Route path="/Page6" component={Page6} />
+              <Route path="/Page7" component={Page7} />
+              <Route path="/Page8" component={Page8} />
+            </Switch>
+
+            {/* <Router/> */}
+
+          </div>
+        </BrowserRouter>
+      </Provider>
+      </div>
     );
   }
 }
 
 export default App;
+
---
+++ frontend/src/components/PageAccueil/Accueil.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './Accueil.css'
+export default class Accueil extends Component {
+  render() {
+    return (
+      <div>
+          <header>
+              <div className="header-nav">
+
+              </div>
+          </header>        
+      </div>
+    )
+  }
+}
