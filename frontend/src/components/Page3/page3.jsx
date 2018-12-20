4bdd442ef57295e5104028a9abde7ea8a6d53615
lydia.dal@live.fr <lydia.dal@live.fr>
2018-12-20 11:35:26 +0100

    page 3 ok

--- frontend/src/components/Page3/page3.jsx
+++ frontend/src/components/Page3/page3.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 
+import image5 from '../../images/image_5.jpg'
+
+import './page3.css'
+
 export default class page3 extends Component {
   render() {
     return (
       <div>
-          <header>
-              <div className="header-nav">
-                
-              </div>
-          </header>
-        
+        <header>
+          <div className="header-nav">
+            <h1 className="title-header">HARMONIA21.ORG</h1>
+          </div>
+        </header>
+        <div className="blue-container">
+          <div class="container">
+            <div class="login-form">        
+              <form id="Login" className="login">
+                <div classN="form-group">
+                  <input type="email" class="form-control" id="inputEmail" placeholder="Adresse Email" />
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe" />
+                </div>
+                <div class="forgot">
+                  <a href="reset.html">Mot de passe oublié?</a>
+                </div>
+                <button type="submit" class="btn btn-primary">ME CONNECTER</button>
+                <button type="submit" class="btn btn-warning btn-compte">Créer un compte</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <img className="image-femme-assise" src={image5} alt="femme-assise"/>
       </div>
     )
   }
--- frontend/src/components/PageAccueil/Accueil.jsx
+++ frontend/src/components/PageAccueil/Accueil.jsx
@@ -8,21 +8,22 @@ export default class Accueil extends Component {
   render() {
     return (
       <div>
-          <header>
-              <div className="header-nav">
-                <h1 className="title-header">HARMONIA21.ORG</h1>
-              </div>
-          </header> 
-          <div className="proverbe">
+        <header>
+          <div className="header-nav">
+            <h1 className="title-header">HARMONIA21.ORG</h1>
+          </div>
+        </header>
+        <div className="proverbe">
           <h2>" Celui qui deplace la montagne, c'est celui qui commence à enlever les petites pierres. "</h2>
-          
           <h3 className="auteur-proverbe">(Confucius)</h3>
-          </div>  
-          <img className="image-enfant" src={image1} alt="enfant"/>     
-          <p>Vous voulez donner une nouvelle destination à vos impôts ?</p>
-          <p>Une dimension humaine !</p>
-          <button type="button">POURQUOI PAS >></button>
-          <p>Impacter le monde !</p>
+        </div>
+        <img className="image-enfant" src={image1} alt="enfant" />
+        <p className="p-top">Vous voulez donner une nouvelle destination à vos impôts ?</p>
+        <p>Une dimension humaine !</p>
+        <div className="bouton-orange">
+          <a href="#" class="btn btn-primary btn-warning">POURQUOI PAS >></a>
+          <p className="p-impacter">Impacter le monde !</p>
+        </div>
       </div>
     )
   }
